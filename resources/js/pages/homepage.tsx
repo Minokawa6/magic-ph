fe457e97a9371b25b02e0c8c048bae4f69d85660
Minokawa <60136630+Minokawa6@users.noreply.github.com>
2026-05-29 16:03:13 +0800

    Early hompage implementation

--- resources/js/pages/homepage.tsx
+++ resources/js/pages/homepage.tsx
@@ -3,6 +3,7 @@ export default function Homepage() {
     return (
         <>
             <Head title="Home"></Head>
+            {/* Navigation */}
             <nav className="flex max-h-20 w-screen flex-row items-center gap-4 bg-white px-8 py-4">
                 <h1 className="text-2xl text-black">Magic Online PH</h1>
                 <div className="relative grow">
@@ -12,7 +13,66 @@ export default function Homepage() {
                 <img className="h-8" src="/images/cart-icon.svg" alt="shopping cart" />
                 <img className="h-8" src="/images/circle-icon.svg" alt="Profile" />
             </nav>
-            <div className="h-screen w-screen bg-amber-200"></div>
+            {/* Content */}
+            <div className="h-full w-full bg-amber-200 p-8">
+                {/* Main Header */}
+                <div className="m-auto flex w-fit items-center justify-center gap-8 rounded-2xl border-2 border-none bg-emerald-500 p-4">
+                    <img className="h-32" src="/images/SOS_expansion_symbol.webp"></img>
+                    <h1 className="h-auto w-32 text-center text-xl font-bold text-wrap text-black">Secrets of Strixhaven</h1>
+                    {/* button */}
+                    <div className="h-fit cursor-pointer rounded-2xl border-2 border-none bg-cyan-500 px-4 py-2 font-bold text-black">
+                        Available Here -{'>'}
+                    </div>
+                </div>
+                <h1 className="text-2xl font-extrabold text-black"> Latest Products</h1>
+                {/* Products Grid */}
+                <div className="m-auto grid grid-cols-4">
+                    {/* Card */}
+                    <div className="m-auto w-fit rounded-2xl border-2 border-none bg-white px-8 py-4">
+                        <h2 className="text-center font-bold text-black"> Lightning Bolt </h2>
+                        <img className="m-auto w-48" src="/images/mtg-cards/2x2-117-lightning-bolt.jpg"></img>
+                        <h3 className="text-sm text-gray-500">{'#'}117</h3>
+                        <h3 className="text-sm text-gray-500">2X2</h3>
+                        <h3 className="text-sm text-gray-500">
+                            Instant{'('}U{')'}
+                        </h3>
+                        <div className="cursor-pointer rounded-sm bg-cyan-500 text-center"> ₱120</div>
+                    </div>
+                    {/* Card */}
+                    <div className="m-auto w-fit rounded-2xl border-2 border-none bg-white px-8 py-4">
+                        <h2 className="text-center font-bold text-black"> Lightning Bolt </h2>
+                        <img className="m-auto w-48" src="/images/mtg-cards/2x2-117-lightning-bolt.jpg"></img>
+                        <h3 className="text-sm text-gray-500">{'#'}117</h3>
+                        <h3 className="text-sm text-gray-500">2X2</h3>
+                        <h3 className="text-sm text-gray-500">
+                            Instant{'('}U{')'}
+                        </h3>
+                        <div className="cursor-pointer rounded-sm bg-cyan-500 text-center"> ₱120</div>
+                    </div>
+                    {/* Card */}
+                    <div className="m-auto w-fit rounded-2xl border-2 border-none bg-white px-8 py-4">
+                        <h2 className="text-center font-bold text-black"> Lightning Bolt </h2>
+                        <img className="m-auto w-48" src="/images/mtg-cards/2x2-117-lightning-bolt.jpg"></img>
+                        <h3 className="text-sm text-gray-500">{'#'}117</h3>
+                        <h3 className="text-sm text-gray-500">2X2</h3>
+                        <h3 className="text-sm text-gray-500">
+                            Instant{'('}U{')'}
+                        </h3>
+                        <div className="cursor-pointer rounded-sm bg-cyan-500 text-center"> ₱120</div>
+                    </div>
+                    {/* Card */}
+                    <div className="m-auto w-fit rounded-2xl border-2 border-none bg-white px-8 py-4">
+                        <h2 className="text-center font-bold text-black"> Lightning Bolt </h2>
+                        <img className="m-auto w-48" src="/images/mtg-cards/2x2-117-lightning-bolt.jpg"></img>
+                        <h3 className="text-sm text-gray-500">{'#'}117</h3>
+                        <h3 className="text-sm text-gray-500">2X2</h3>
+                        <h3 className="text-sm text-gray-500">
+                            Instant{'('}U{')'}
+                        </h3>
+                        <div className="cursor-pointer rounded-sm bg-cyan-500 text-center"> ₱120</div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
